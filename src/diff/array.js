51b84e3bba02aeb39ced6d8e0dc797700a58be98
Chris Goodwin <ceilingazn@gmail.com>
2017-07-18 16:37:39 +1200

    Add comparator option for use with ArrayDiff

--- src/diff/array.js
+++ src/diff/array.js
@@ -4,5 +4,15 @@ export const arrayDiff = new Diff();
 arrayDiff.tokenize = arrayDiff.join = function(value) {
   return value.slice();
 };
+arrayDiff.equals = function(left, right) {
+  // Checks if we have a comparator defined in the options and
+  // executes it otherwise fallback to the default comparator
+  if (typeof this.options.comparator === 'function') {
+    return this.options.comparator(left, right);
+  } else {
+    return left === right
+      || (this.options.ignoreCase && left.toLowerCase() === right.toLowerCase());
+  }
+};
 
 export function diffArrays(oldArr, newArr, callback) { return arrayDiff.diff(oldArr, newArr, callback); }
